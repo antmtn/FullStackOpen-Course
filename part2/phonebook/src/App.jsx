b2a5807c52c335826bc313009e1d3f0cce441962
antmtn <116135015+notnuthin@users.noreply.github.com>
2026-07-31 22:23:21 -0700

    2.11, added db file, json-server for dev, and added effect for fetching initial people state from the db

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
 
 const SearchFilter = (props) => {
   return(
@@ -33,15 +35,18 @@ const PeopleDisplay = (props) =>
   ))
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newPhoneNumber, setNewPhoneNumber] = useState('')
   const [nameFilter, setNewNameFilter] = useState('')
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response =>{
+      setPersons(response.data)
+    })
+  },[])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
