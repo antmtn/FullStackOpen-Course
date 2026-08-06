5641bb908a8554e1853e13ed43c22f871144b824
antmtn <116135015+notnuthin@users.noreply.github.com>
2026-08-05 17:07:00 -0700

    3.5 added post endpoint for new person with field checking

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -57,6 +57,23 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+app.post('/api/persons', (request, response)=> {
+  const id = Math.floor(Math.random()*1000) + 5
+  const body = request.body
+
+  if(!body.name || !body.number){
+    return response.status(404).send('Name or number missing')
+  }
+
+  const person = {
+    id: id,
+    name: body.name,
+    number: body.number
+  }
+  persons = persons.concat(person)
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
