b11464847fb1adece19b589fce3906d56ed1a079
antmtn <116135015+notnuthin@users.noreply.github.com>
2026-08-06 16:04:32 -0700

    3.9 Frontend from part2/phonebook now works with backend

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 var morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
