ac3f59c9ebcd68a741bdf6d0aec0097bb32f3ca5
antmtn <116135015+notnuthin@users.noreply.github.com>
2026-08-09 00:20:09 -0700

    3.15, 3.16 Integrated delete with DB, Added error handler middleware, added proxy to vite config from part 3b.proxy and removed cors

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -2,13 +2,11 @@ require('dotenv').config()
 const Person = require('./models/person')
 const express = require('express')
 var morgan = require('morgan')
-const cors = require('cors')
 
 
 const app = express()
 
 app.use(express.static('dist'))
-app.use(cors())
 app.use(express.json())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -64,14 +62,11 @@ app.get('/info', (request,response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const prevLen = persons.length
-  persons = persons.filter((person) => person.id !== id)
-
-  if (prevLen === persons.length)
-    response.status(404).send(`Person with id ${id} not found`)
-  
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response)=> {
@@ -94,6 +89,18 @@ app.post('/api/persons', (request, response)=> {
     response.json(savedPerson)
   })
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
