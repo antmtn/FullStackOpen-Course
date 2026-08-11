2177c491742256166e9b0e904a64fd6b69bb31ac
antmtn <116135015+notnuthin@users.noreply.github.com>
2026-08-11 15:03:14 -0700

    4.2 Blog App refactored to follow best practice repository structure

---
+++ part4/app.js
@@ -0,0 +1,21 @@
+const config = require('./utils/config')
+const express = require('express')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { family: 4 })
+.then(() => {
+  console.log('Connected to MongoDB')
+})
+.catch(error => {
+  console.log('Error connecting to MongoDB:', error.message)
+})
+
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
