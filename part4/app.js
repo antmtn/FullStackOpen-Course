c9481ef97688655a93e142ed9e0a637c14f6fa45
antmtn <116135015+notnuthin@users.noreply.github.com>
2026-08-17 15:09:47 -0700

    4.8 wrote test for fetching all blogs using SuperTest and cross env (for diff test env and DB) and fixed config.js variable name mismatch

--- part4/app.js
+++ part4/app.js
@@ -6,6 +6,7 @@ const blogsRouter = require('./controllers/blogs')
 const app = express()
 
 const mongoUrl = config.MONGODB_URI
+console.log("mongoUrl", mongoUrl)
 mongoose.connect(mongoUrl, { family: 4 })
 .then(() => {
   console.log('Connected to MongoDB')
--- part4/utils/config.js
+++ part4/utils/config.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.NODE_ENV === 'test' 
-? process.env.TEST_MONGODB_URI
-: process.env.MONGODB_URI
+
+const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.MONGODB_TEST_URI : process.env.MONGODB_URI
+
+console.log("MongoDB_URI in config.js", MONGODB_URI)
+console.log("process.env.NODE_ENV", process.env.NODE_ENV)
 
 module.exports = { MONGODB_URI, PORT }
