8649cddfcf1dd7925b52cf00e5368409494cc9d8
antmtn <116135015+notnuthin@users.noreply.github.com>
2026-08-21 20:42:34 -0700

    fixed bug saving blogs to wrong user attribute

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -27,7 +27,7 @@ blogsRouter.post('/', async (request, response) => {
   })
 
   const savedBlog = await blog.save()
-  user.notes = user.blogs.concat(savedBlog._id)
+  user.blogs = user.blogs.concat(savedBlog._id)
   await user.save()
   response.status(201).json(savedBlog)
 })
