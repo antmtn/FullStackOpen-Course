c49e38d4234f27c9b58a0478480f5308e62815dc
antmtn <116135015+notnuthin@users.noreply.github.com>
2026-08-25 23:13:41 -0700

    fixed typo in error handler and added username and password form to frontend

--- part5/backend/utils/middleware.js
+++ part5/backend/utils/middleware.js
@@ -27,7 +27,7 @@ const errorHandler = (error, request, response, next) => {
   ) {
     return response
       .status(400)
-      .json({ error: 'expeceted username to be unique' })
+      .json({ error: 'expected username to be unique' })
   } else if (error.name === 'JsonWebTokenError') {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
--- part5/frontend/src/App.jsx
+++ part5/frontend/src/App.jsx
@@ -4,6 +4,8 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +13,40 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+  }
+
   return (
     <div>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+
+      <h2>Login</h2>
+      <form onSubmit = {handleLogin}>
+        <div>
+          <label>
+            username
+            <input
+              type ="text"
+              value ={username}
+              onChange={({target}) => setUsername(target.value)}
+            />
+          </label>
+        </div>
+          <label>
+            password
+            <input
+              type="password"
+              value={password}
+              onChange={({target}) => setPassword(target.value)}
+            />
+          </label>
+          <button type="submit">login</button>
+      </form>
     </div>
   )
 }
