0b641e06d1659a568f100a0f5f4938f39b52df9c
antmtn <116135015+notnuthin@users.noreply.github.com>
2026-08-27 00:10:41 -0700

    5.2 logout added, login functionality is now permanent using localStorage

--- part5/frontend/src/App.jsx
+++ part5/frontend/src/App.jsx
@@ -16,6 +16,19 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON){
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  }
+
   const handleLogin = async (event) => {
     event.preventDefault()
     
@@ -24,6 +37,9 @@ const App = () => {
        setUser(user)
        setUsername('')
        setPassword('')
+       window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+       )
     } catch {
       alert("wrong credentials")
     }
@@ -47,7 +63,10 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>
+        {user.name} logged in
+        <button onClick={handleLogout}>Logout</button>
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
