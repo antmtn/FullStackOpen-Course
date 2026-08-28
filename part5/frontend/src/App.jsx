4e4aed0e904e08d841b6f22c51c2e7fd4bea0fc6
antmtn <116135015+notnuthin@users.noreply.github.com>
2026-08-27 20:52:07 -0700

    5.3 logged-in user can add new blogs

--- part5/frontend/src/App.jsx
+++ part5/frontend/src/App.jsx
@@ -3,12 +3,16 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import LoginForm from './components/LoginForm'
+import BlogForm from './components/BlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const[user, setUser] = useState(null)
+  const [titleField, setTitleField] = useState('')
+  const [authorField, setAuthorField] = useState('')
+  const [urlField, setUrlField] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -21,8 +25,33 @@ const App = () => {
     if (loggedUserJSON){
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
+
+  const addBlog = async event => {
+    event.preventDefault()
+    const blogObject = {
+      title:titleField,
+      author:authorField,
+      url:urlField
+    }
+    setTitleField('')
+    setAuthorField('')
+    setUrlField('')
+    const returnedBlog = await blogService.create(blogObject)
+    setBlogs(blogs.concat(returnedBlog))
+  }
+
+  const handleTitleChange = event => {
+    setTitleField(event.target.value)
+  }
+  const handleAuthorChange = event => {
+    setAuthorField(event.target.value)
+  }
+  const handleUrlChange = event => {
+    setUrlField(event.target.value)
+  }
 
   const handleLogout = () => {
     window.localStorage.removeItem('loggedBlogappUser')
@@ -40,6 +69,7 @@ const App = () => {
        window.localStorage.setItem(
         'loggedBlogappUser', JSON.stringify(user)
        )
+       blogService.setToken(user.token)
     } catch {
       alert("wrong credentials")
     }
@@ -67,6 +97,17 @@ const App = () => {
         {user.name} logged in
         <button onClick={handleLogout}>Logout</button>
       </p>
+
+      <h2>create new</h2>
+      <BlogForm
+        addBlog={addBlog}
+        titleField={titleField}
+        handleTitleChange = {handleTitleChange}
+        authorField={authorField}
+        handleAuthorChange={handleAuthorChange}
+        urlField={urlField}
+        handleUrlChange={handleUrlChange}
+      />
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
---
+++ part5/frontend/src/components/BlogForm.jsx
@@ -0,0 +1,24 @@
+const BlogForm = ({addBlog, titleField, handleTitleChange, authorField, handleAuthorChange, urlField, handleUrlChange}) => {
+  return (
+    <form onSubmit = {addBlog}>
+        title:
+        <input 
+          value={titleField}
+          onChange={handleTitleChange}
+        /><br/>
+        author:
+        <input
+          value={authorField}
+          onChange={handleAuthorChange}
+        /><br/>
+        url:
+        <input
+          value={urlField}
+          onChange={handleUrlChange}
+        /><br/>
+        <button >create</button>
+      </form>
+  )
+}
+
+export default BlogForm
