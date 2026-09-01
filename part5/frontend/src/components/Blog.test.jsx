cb131f77eb82358426c3fdd7bdc137db7fc29c03
antmtn <116135015+notnuthin@users.noreply.github.com>
2026-08-31 18:33:39 -0700

    5.14 test for ensuring visibility of URL and Likes of blog after clicking view button implemented

--- part5/frontend/src/components/Blog.test.jsx
+++ part5/frontend/src/components/Blog.test.jsx
@@ -27,3 +27,31 @@ test('renders content', () => {
   expect(likes).not.toBeVisible()
 
 })
+
+test('content shown after clicking view button', async() => {
+  const sampleUser = {
+    name:'Joseph A. Test',
+    username:'Joe T'
+  }
+  const blog = {
+    title: 'Component testing',
+    author: 'Joseph Test',
+    url: 'Joseph.com',
+    likes: '67',
+    user:{
+      name:'Joseph A. Test',
+      username:'Joe T'
+    }
+  }
+  render(<Blog blog={blog} user={sampleUser} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const url = screen.getByText('Joseph.com', {exact: false})
+  const likes = screen.getByText('likes', {exact: false})
+
+  expect(url).toBeVisible()
+  expect(likes).toBeVisible()
+})
