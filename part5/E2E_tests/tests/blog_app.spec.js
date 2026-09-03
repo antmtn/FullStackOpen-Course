109c628e01b558ce44e4d0a3eea68efcaf6f7c2a
antmtn <116135015+notnuthin@users.noreply.github.com>
2026-09-02 21:25:29 -0700

    bug caused by mismatching ports in config.js, vite proxy, and test post URLs fixed, test for successful login working, helper added(not used)

--- part5/E2E_tests/tests/blog_app.spec.js
+++ part5/E2E_tests/tests/blog_app.spec.js
@@ -1,7 +1,15 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3001/api/testing/reset')
+    await request.post('http://localhost:3001/api/users', {
+      data: {
+        name: 'Pear Shah',
+        username: 'pear.shah',
+        password: 'Shah'
+      }
+    })
     await page.goto('http://localhost:5173')
   })
 
@@ -12,5 +20,16 @@ describe('Blog app', () => {
     await expect(title).toBeVisible()
     await expect(username).toBeVisible()
     await expect(password).toBeVisible()
+  })
+
+  test('User can log in', async ({ page }) => {
+    await page.getByLabel('username').fill('pear.shah')
+    await page.getByLabel('password').fill('Shah')
+    await page.getByRole('button', { name:'login' }).click()
+    await expect(page.getByText('Pear Shah logged in')).toBeVisible()
+  })
+
+  test('login fails with wrong password', async ({ page }) => {
+
   })
 })
